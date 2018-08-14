311fc621d176281ea82731a994ddf58f5eaa9aa5
Chien Do <chien.dotruong@gmail.com>
2018-08-14 19:07:58 +0700

    - Test

--- src/ControlFan.py
+++ src/ControlFan.py
@@ -23,47 +23,47 @@ SleepTimeL = 300
 # main loop
 
 def main():
-	i = 0
-	while True:
-		if i < 12:
-			GPIO.output(18, GPIO.LOW)
-			print "i = " ,i, ", Fan 1 is running. Fan 2 off"
-		else:
-		  	GPIO.output(18, GPIO.HIGH)
-			print "i = " ,i, ", Fan 1 off. Fan 2 is running"
-
-
-		#--------------------------------------------
-		humidity, temperature = Adafruit_DHT.read_retry(22, 4)
-		time.sleep(2);
-		humidity, temperature = Adafruit_DHT.read_retry(22, 4)
-		if humidity is not None and temperature is not None:
-			print(str(datetime.datetime.now()) + ' -- Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-		else:
-			print('Failed to get reading. Try again!')
-
-		#if temperature > 32.5 or humidity > 90:
-        if temperature > 32.5:
-        	GPIO.output(17, GPIO.LOW)
-        	print('Temp or Humidity is too high. Start Fan 3')
+    i = 0
+while True:
+        if i < 12:
+            GPIO.output(18, GPIO.LOW)
+            print "i = " ,i, ", Fan 1 is running. Fan 2 off"
         else:
-        	GPIO.output(17, GPIO.HIGH)
-        	print('Turn off Fan 3')
+              GPIO.output(18, GPIO.HIGH)
+            print "i = " ,i, ", Fan 1 off. Fan 2 is running"
 
-		i += 1	#Test
-		time.sleep(SleepTimeL);
-		print('-----------')
-		if i > 24:
-			i = 0
+
+        #--------------------------------------------
+        humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+        time.sleep(2);
+        humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+        if humidity is not None and temperature is not None:
+            print(str(datetime.datetime.now()) + ' -- Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+
+        #if temperature > 32.5 or humidity > 90:
+        if temperature > 32.5:
+            GPIO.output(17, GPIO.LOW)
+            print('Temp or Humidity is too high. Start Fan 3')
+        else:
+            GPIO.output(17, GPIO.HIGH)
+            print('Turn off Fan 3')
+
+        i += 1    #Test
+        time.sleep(SleepTimeL);
+        print('-----------')
+        if i > 24:
+            i = 0
 
 def destroy():
     GPIO.output(pinList, GPIO.LOW)
     GPIO.cleanup()
 
 try:
-	main()
+    main()
 except KeyboardInterrupt:
-	# End program cleanly with keyboard
-  	print " Quit"
-  	# Reset GPIO settings
-	destroy()
+    # End program cleanly with keyboard
+      print " Quit"
+      # Reset GPIO settings
+    destroy()
