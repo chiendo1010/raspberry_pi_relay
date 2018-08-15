0d4c4dc55866185490a9182a7148c5991b8209b0
Chien Do <chien.dotruong@gmail.com>
2018-08-15 20:39:15 +0700

    - Update.

--- src/google_spreadsheet.py
+++ src/google_spreadsheet.py
@@ -81,7 +81,11 @@ FREQUENCY_SECONDS      = 30
 def login_open_sheet(oauth_key_file, spreadsheet):
     """Connect to Google Docs spreadsheet and return the first worksheet."""
     try:
-        scope =  ['https://spreadsheets.google.com/feeds']
+        # scope =  ['https://spreadsheets.google.com/feeds']
+        scope=[
+            'https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive'
+        ]
         print('Debug 11111')
         credentials = ServiceAccountCredentials.from_json_keyfile_name(oauth_key_file, scope)
         print('Debug 22222')
